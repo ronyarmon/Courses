bf7fc90ba86523af85bdf61735e71548fe35c3c1
ronydev50@gmail.com <ronydev50@gmail.com>
2023-07-30 15:11:41 +0300

    postgres tests fix

--- image_setup/test/scripts/db_tables.py
+++ image_setup/test/scripts/db_tables.py
@@ -1,20 +1,27 @@
 import pandas as pd
 from sqlalchemy import create_engine
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 db_name, db_user, db_password, db_host = 'testdb', 'postgres', 'postgres', 'localhost'
+conn = psycopg2.connect(database=db_name, user=db_user, password=db_password,\
+                         host=db_host, port='5432')
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+cur = conn.cursor()
 engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_password}@{db_host}/{db_name}',\
                        pool_pre_ping=True)
 # Create table
-engine.execute('DROP TABLE IF EXISTS users')
-engine.execute("""
+cur.execute('DROP TABLE IF EXISTS users')
+cur.execute("""
              CREATE TABLE IF NOT EXISTS users(
              id SERIAL PRIMARY KEY,
              name VARCHAR (50) NOT NULL,
              email VARCHAR (50) UNIQUE NOT NULL) 
              """)
-engine.execute("""INSERT INTO users
+cur.execute("""INSERT INTO users
                        (name, email)
                        VALUES ('jack', 'jack@gogo.com')""")
-print(list(engine.execute("""SELECT * FROM users""")))
+cur.execute("""SELECT * FROM users""")
+print(cur.fetchall())
 names_emails = {'name':['john', 'jean'], 'email':['john@gogo.com','jean@gogo.com']}
 users_df = pd.DataFrame.from_dict(names_emails)
 print(users_df)
